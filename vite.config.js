e852c26f6f297d90ebb207aa2ed366725388eb73
QuispeBrianGabriel <quispebriangabriel@gmail.com>
2026-07-15 22:17:40 -0300

    chore(config): covertura minima de 90%

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,31 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { configDefaults } from 'vitest/config';
 
 export default defineConfig({
   plugins: [react()],
+
+  test: {
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html', 'lcov'],
+      reportsDirectory: 'coverage',
+      all: true,
+      include: ['src/**/*.{ts,tsx}'],
+      exclude: [
+        ...configDefaults.exclude,
+        'src/main.tsx',
+        '**/*.d.ts',
+        '**/*.feature',
+        '**/*.stories.*',
+        '**/index.ts',
+      ],
+      thresholds: {
+        lines: 90,
+        branches: 90,
+        functions: 90,
+        statements: 90,
+      },
+    },
+  },
 });
